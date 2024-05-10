67ff8155b02da4dbc5166695d23444b3fc66a094
Slesa Adhikari <slesaad@gmail.com>
2024-05-10 15:17:42 -0500

    Add support for a timestamp

--- docker_tasks/build_stac/utils/stac.py
+++ docker_tasks/build_stac/utils/stac.py
@@ -2,6 +2,7 @@ import os
 
 import pystac
 import rasterio
+import requests
 from rasterio.session import AWSSession
 from rio_stac import stac
 
@@ -10,6 +11,46 @@ from . import events, regex, role
 
 PROJECTION_EXT_VERSION = "v1.1.0"
 RASTER_EXT_VERSION = "v1.1.0"
+
+
+class SessionWithHeaderRedirection(requests.Session):
+    AUTH_HOST = "urs.earthdata.nasa.gov"
+
+    def __init__(self, username, password):
+        super().__init__()
+        self.auth = (username, password)
+
+    # Overrides from the library to keep headers when redirected to or from
+    # the NASA auth host.
+    def rebuild_auth(self, prepared_request, response):
+        headers = prepared_request.headers
+        url = prepared_request.url
+        if "Authorization" in headers:
+            original_parsed = requests.utils.urlparse(response.request.url)
+            redirect_parsed = requests.utils.urlparse(url)
+            if (
+                (original_parsed.hostname != redirect_parsed.hostname)
+                and redirect_parsed.hostname != self.AUTH_HOST
+                and original_parsed.hostname != self.AUTH_HOST
+            ):
+                del headers["Authorization"]
+        return
+
+
+def fetch_temp_credentials():
+    print("fetching temp creds")
+    session = SessionWithHeaderRedirection()
+    response = session.get("https://data.lpdaac.earthdatacloud.nasa.gov/s3credentials")
+    access_key_id = response.json()["accessKeyId"]
+    secret_access_key = response.json()["secretAccessKey"]
+    session_token = response.json()["sessionToken"]
+
+    print(access_key_id, secret_access_key, session_token)
+    return AWSSession(
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+        aws_session_token=session_token,
+    )
 
 
 def get_sts_session():
@@ -98,7 +139,9 @@ def generate_stac(event: events.RegexEvent) -> pystac.Item:
     assets = {}
 
     rasterio_kwargs = {}
-    rasterio_kwargs["session"] = get_sts_session()
+    rasterio_kwargs["session"] = fetch_temp_credentials()
+    print(rasterio_kwargs)
+
     with rasterio.Env(
         session=rasterio_kwargs.get("session"),
         options={**rasterio_kwargs},
